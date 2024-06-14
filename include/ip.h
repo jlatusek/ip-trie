a10621abd6cb020f74c8d5d684cd35f03ec4bde4
Jakub Latusek <jk.latusek@gmail.com>
2024-06-14 23:40:21 +0200

    Change arg to const

--- include/ip.h
+++ include/ip.h
@@ -7,4 +7,4 @@ constexpr int IP_ERROR = -1;
 constexpr unsigned int IP_LEN = 32;
 
 uint32_t str_to_ip(const char *ip_str);
-uint32_t numb_to_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4);
+uint32_t numb_to_ip(const uint8_t ip1, const uint8_t ip2, const uint8_t ip3, const uint8_t ip4);
--- src/ip.c
+++ src/ip.c
@@ -9,7 +9,7 @@ uint32_t str_to_ip(const char *ip_str)
     return numb_to_ip(c[0], c[1], c[2], c[3]);
 }
 
-uint32_t numb_to_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4)
+uint32_t numb_to_ip(const uint8_t ip1, const uint8_t ip2, const uint8_t ip3, const uint8_t ip4)
 {
     return ip1 << 24 | ip2 << 16 | ip3 << 8 | ip4;
 }
