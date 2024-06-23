8056f71bf508a064457687fd7b319faf2aa05887
Jakub Latusek <jk.latusek@gmail.com>
2024-06-23 13:29:55 +0200

    Clean const

--- include/ip_tool.h
+++ include/ip_tool.h
@@ -3,5 +3,5 @@
 #include <stdint.h>
 
 int str_to_ip(const char *ip_str, uint32_t *ip);
-uint32_t numb_to_ip(const uint8_t ip1, const uint8_t ip2, const uint8_t ip3, const uint8_t ip4);
-int ip_to_str(const uint32_t ip, char *ip_str, int len);
+uint32_t numb_to_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4);
+int ip_to_str(uint32_t ip, char *ip_str, int len);
